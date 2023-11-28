312b7b8ec37801e4a7bcbde9793af93d83791e26
pavankumar <74093393+pavan1192@users.noreply.github.com>
2023-11-28 12:24:38 +0530

    Create credentials.js

---
+++ credentials.js
@@ -0,0 +1,5 @@
+const CLIENT_ID = "765843530641-hoktemhfu14t1op19cq0sooukv1djqdu.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN ="1//04Dn9aeHYlZuvCgYIARAAGAQSNwF-L9IrOqrKmAJEIvEGg54tenfyDOTQDRQe97jS5zWFmB5YNevzD3fnzNwvlr-gIUmLMbUr0-o";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
