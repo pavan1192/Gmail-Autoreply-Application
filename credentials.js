6ef4dde94f2fa214ed64e6e49c5f1d72b49d6790
pavankumar <74093393+pavan1192@users.noreply.github.com>
2023-11-28 15:35:56 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = "765843530641-hoktemhfu14t1op19cq0sooukv1djqdu.apps.googleusercontent.com";
-const CLEINT_SECRET = "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";
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN ="1//04Dn9aeHYlZuvCgYIARAAGAQSNwF-L9IrOqrKmAJEIvEGg54tenfyDOTQDRQe97jS5zWFmB5YNevzD3fnzNwvlr-gIUmLMbUr0-o";
+const CLIENT_ID = "your client id";
+const CLEINT_SECRET = "your client secret";
+const REDIRECT_URI = "redirect uri of yours";
+const REFRESH_TOKEN ="your uri token";
 module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
